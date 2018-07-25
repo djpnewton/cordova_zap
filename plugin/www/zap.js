d68af86513b1293f9ea7e6c4ce80b0dc81c05f88
Daniel Newton <djpnewton@gmail.com>
2018-07-25 12:12:27 +1200

    initial code

---
+++ plugin/www/zap.js
@@ -0,0 +1,7 @@
+var exec = require('cordova/exec');
+
+var PLUGIN_NAME = 'zap';
+
+exports.coolMethod = function (arg0, success, error) {
+    exec(success, error, PLUGIN_NAME, 'coolMethod', [arg0]);
+};
