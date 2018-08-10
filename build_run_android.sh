f3da0c7f84273ebed4722180e108d9fd580f4c41
Daniel Newton <djpnewton@gmail.com>
2018-08-10 12:13:43 +1200

    update libzap.. improve mac support

--- build_run_android.sh
+++ build_run_android.sh
@@ -2,6 +2,22 @@
 
 set -e
 
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=Mac;;
+    CYGWIN*)    machine=Cygwin;;
+    MINGW*)     machine=MinGw;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
+echo $machine
+
+if [ "$machine" == "Mac" ]; then
+    export ANDROID_SDK=~/Library/Android/sdk
+    export ANDROID_NDK=~/Library/Android/sdk/ndk-bundle
+    export PATH="$PATH:$ANDROID_SDK/tools:$ANDROID_SDK/platform-tools:$ANDROID_NDK"
+fi
+
 echo :: build libzap.so
 (cd libzap/src; ./build_android_makefiles.sh; make)
 
--- build_run_android_mac.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-export ANDROID_SDK=~/Library/Android/sdk
-export ANDROID_NDK=~/Library/Android/sdk/ndk-bundle
-export PATH="$PATH:$ANDROID_SDK/tools:$ANDROID_SDK/platform-tools:$ANDROID_NDK"
-
-./build_run_android.sh
